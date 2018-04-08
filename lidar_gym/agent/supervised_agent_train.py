f162848876a78636f28382c21867dba80d3fb717
Zdeněk Rozsypálek <rozsyzde@fel.cvut.cz>
2018-04-09 01:04:27 +0200

    keras model update

--- lidar_gym/agent/supervised_agent_train.py
+++ lidar_gym/agent/supervised_agent_train.py
@@ -51,25 +51,28 @@ def append_to_buffer(obs):
 
 def build_network():
     # 3D convolutional network building
-    model = keras.models.Sequential()
-    model.add(keras.layers.Conv3D(2, 4, padding='same', kernel_regularizer=keras.regularizers.l2(0.01),
-                                  input_shape=[MAP_SIZE[0], MAP_SIZE[1], MAP_SIZE[2], 1], activation='relu'))
-    model.add(keras.layers.Conv3D(4, 4, padding='same', kernel_regularizer=keras.regularizers.l2(0.01),
-                                  activation='relu'))
-    model.add(keras.layers.MaxPooling3D(pool_size=2))
-    model.add(keras.layers.Conv3D(8, 4, padding='same', kernel_regularizer=keras.regularizers.l2(0.01),
-                                  activation='relu'))
-    model.add(keras.layers.MaxPooling3D(pool_size=2))
-    model.add(keras.layers.Conv3D(16, 4, padding='same', kernel_regularizer=keras.regularizers.l2(0.01),
-                                  activation='relu'))
-    model.add(keras.layers.Conv3D(32, 4, padding='same', kernel_regularizer=keras.regularizers.l2(0.01),
-                                  activation='relu'))
-    model.add(keras.layers.Conv3D(1, 8, padding='same', kernel_regularizer=keras.regularizers.l2(0.01),
-                                  activation='linear'))
-    model.add(keras.layers.Conv3DTranspose(1, 8, strides=[4, 4, 4], padding='same', activation='linear',
-                                           kernel_regularizer=keras.regularizers.l2(0.01)))
+    inputs = keras.layers.Input(shape=(MAP_SIZE[0], MAP_SIZE[1], MAP_SIZE[2]))
+    reshape = tf.expand_dims(inputs, -1)
 
-    return model
+    c1 = keras.layers.Conv3D(2, 4, padding='same', kernel_regularizer=keras.regularizers.l2(0.01),
+                             activation='relu')(reshape)
+    c2 = keras.layers.Conv3D(4, 4, padding='same', kernel_regularizer=keras.regularizers.l2(0.01),
+                             activation='relu')(c1)
+    p1 = keras.layers.MaxPooling3D(pool_size=2)(c2)
+    c3 = keras.layers.Conv3D(8, 4, padding='same', kernel_regularizer=keras.regularizers.l2(0.01),
+                             activation='relu')(p1)
+    p2 = keras.layers.MaxPooling3D(pool_size=2)(c3)
+    c4 = keras.layers.Conv3D(16, 4, padding='same', kernel_regularizer=keras.regularizers.l2(0.01),
+                             activation='relu')(p2)
+    c5 = keras.layers.Conv3D(32, 4, padding='same', kernel_regularizer=keras.regularizers.l2(0.01),
+                             activation='relu')(c4)
+    c6 = keras.layers.Conv3D(1, 8, padding='same', kernel_regularizer=keras.regularizers.l2(0.01),
+                             activation='linear')(c5)
+    outputs = keras.layers.Conv3DTranspose(1, 8, strides=[4, 4, 4], padding='same', activation='linear',
+                                           kernel_regularizer=keras.regularizers.l2(0.01))(c6)
+    outputs = tf.squeeze(outputs, [4])
+
+    return keras.models.Model(inputs, outputs)
 
 '''
 def build_network():
