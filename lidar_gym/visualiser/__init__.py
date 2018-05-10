4dd0c75d6565a83fc90ad51c2ab589b9afc0d7df
Zdeněk Rozsypálek <rozsyzde@fel.cvut.cz>
2018-05-10 12:59:03 +0200

    ASCII rendering added

--- lidar_gym/visualiser/__init__.py
+++ lidar_gym/visualiser/__init__.py
@@ -1,1 +1,7 @@
-from lidar_gym.visualiser import plot3d, printer
+from lidar_gym.visualiser import printer
+import importlib
+mayavi_spec = importlib.util.find_spec("mayavi")
+found = mayavi_spec is not None
+
+if found:
+    from lidar_gym.visualiser import plot3d
